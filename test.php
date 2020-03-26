e30ec2cf7647346245d43bcab36c9359f63721d0
lsjhtang <157618647@qq.com>
2020-03-27 00:29:50 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,49 @@
 <?php
-require_once __DIR__. '/vendor/autoload.php';
-require_once __DIR__.'/app/config/define.php';
 
-\Core\BeanFactory::init();
-$user = \Core\BeanFactory::getBean('user');
-$users = \Core\BeanFactory::getBean('RouterCollector');
-var_dump($user,$users);
-exit();
+use Swoole\Http\Server;
+
+if ($argc == 2) {
+    $cmd = $argv[1];
+
+    if ($cmd == 'start') {
+        startSwoole();
+    } elseif ($cmd == 'stop') {
+        killSwoole();
+    }elseif ($cmd == 'restart'){
+        $re =   killSwoole();
+        echo $re.PHP_EOL;
+        if($re){
+            sleep(1);
+            echo '启动成功'.PHP_EOL;
+            startSwoole();
+        }else{
+            echo '重启失败!'.PHP_EOL;
+        }
+    }
+}
+
+function startSwoole () {
+    $http = new Swoole\Http\Server('0.0.0.',80);
+    $http->set([
+        'worker_num'=>1,
+        'daemonize'=>false,
+    ]);
+    $http->on('request',function($req,$res){
+
+    });
+    $http->on('Start',function(Server $server){
+        $mid = $server->master_pid;
+        file_put_contents('./top.pid',$mid);
+    });
+    $http->start();
+}
+
+function killSwoole(){
+    $mid = intval(file_get_contents('./top.pid'));
+    if ($mid && trim($mid) !=0){
+       $re =  \Swoole\Process::kill($mid);
+       return $re;
+    }else{
+        return false;
+    }
+}
